2e1c15270d68c1f5206d0d32d6d851d2120f8a48
GabrielFDF <gabrielfailache@gmail.com>
2026-09-08 20:06:47 -0300

    Add files via upload

---
+++ trabalho de media].py
@@ -0,0 +1,6 @@
+nota_1 = float(input('digite sua primeira nota; '))
+nota_2 = float(input('digite seua segunda nota; '))
+nota_3 = float(input('digite sua terçeira nota; '))
+nota_4 = float(input('digite sua quarta nota; '))
+media = (nota_1 + nota_2 + nota_3 + nota_4) / 4
+print (f"a media das quatros notas é: {media}")
